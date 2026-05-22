400f44509ee9910452a66eb5f089dc3121b6aff6
vamshikrushnaakula <96808439+vamshikrishnaakula@users.noreply.github.com>
2026-05-21 23:26:50 -0400

    hero section

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import type {} from "@tanstack/react-start";
 
-const BASE_URL = "https://ritetechconstruction.com";
+const BASE_URL = "";
 
 export const Route = createFileRoute("/sitemap.xml")({
   server: {
