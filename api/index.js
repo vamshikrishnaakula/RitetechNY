a77906a72ab1805c346246f81ce920c35f6e01ed
vamshikrushnaakula <96808439+vamshikrishnaakula@users.noreply.github.com>
2026-05-23 11:49:39 -0400

    m

--- api/index.js
+++ api/index.js
@@ -1,11 +1,7 @@
-import { fileURLToPath } from "node:url";
-import { dirname, join } from "node:path";
+import server from "../src/server.ts";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const serverPath = join(__dirname, "../src/server.ts");
-const serverModule = await import(serverPath);
-const server = serverModule.default ?? serverModule;
+const app = server.default ?? server;
 
 export default async function handler(request) {
-  return server.fetch(request, undefined, undefined);
+  return app.fetch(request, undefined, undefined);
 }
